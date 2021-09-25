96249c47e0bedebee23cf6ce7c1489003b058180
Jaime Zapata <zapataval2304@gmail.com>
2021-09-25 16:01:40 -0500

    Se cambian las variables de TennisGame3 y se refactoriza

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,34 +1,52 @@
 public class TennisGame3 implements TennisGame {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int scorePlayer2;
+    private int scorePlayer1;
+    private String player1Name;
+    private String player2Name;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        if (scorePlayer1 < 4 && scorePlayer2 < 4 && scorePlayer1 + scorePlayer2 < 6) {
+            return midGameScore();
         }
+        return winnerOrLead();
+
+    }
+
+    private String winnerOrLead() {
+        String score;
+        if (scorePlayer1 == scorePlayer2)
+            return "Deuce";
+        score = whichIsGrater();
+        return winnerOrLead(score);
+    }
+
+    private String winnerOrLead(String score) {
+        return ((scorePlayer1 - scorePlayer2) * (scorePlayer1 - scorePlayer2) == 1) ? "Advantage " + score : "Win for " + score;
+    }
+
+    private String whichIsGrater(){
+        String score;
+        return scorePlayer1 > scorePlayer2 ? player1Name : player2Name;
+    }
+
+    private String midGameScore() {
+        String score;
+        String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        score = p[scorePlayer1];
+        return (scorePlayer1 == scorePlayer2) ? score + "-All" : score + "-" + p[scorePlayer2];
     }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.scorePlayer1 += 1;
         else
-            this.p2 += 1;
+            this.scorePlayer2 += 1;
 
     }
 
