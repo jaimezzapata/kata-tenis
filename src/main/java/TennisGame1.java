5daa617fb6ce83ed92e5e2206f8572e8a46a70c1
Jaime Zapata <zapataval2304@gmail.com>
2021-09-25 15:55:51 -0500

    Se hace refactorización de TennisGame1

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,7 +1,7 @@
 public class TennisGame1 implements TennisGame {
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1_Score = 0;
+    private int player2_Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -12,64 +12,99 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            player1_Score += 1;
         else
-            m_score2 += 1;
+            player2_Score += 1;
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        String result="";
+        if (player1_Score == player2_Score)
         {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+            result= tieScore();
+        }
+        else if (player1_Score >=4 || player2_Score >=4)
+        {
+            result= closeVictoryOrWining();
+        }else{
+            result= sideAdvantage();
 
-            }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        return result;
+
+    }
+
+    private String sideAdvantage() {
+        String score= "";
+        for (int i = 1; i<3; i++)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score = comparateAdbantage(score, i);
         }
         return score;
+    }
+
+    private String comparateAdbantage(String score, int i) {
+        int tempScore;
+        if (i ==1) {
+            tempScore = player1_Score;
+        }
+        else {
+            score +="-"; tempScore = player2_Score;
+        }
+        score = sideAdvantageCases(tempScore, score);
+        return score;
+    }
+
+    private String sideAdvantageCases(int tempScore, String score) {
+        switch(tempScore)
+        {
+            case 0:
+                score +="Love";
+                break;
+            case 1:
+                score +="Fifteen";
+                break;
+            case 2:
+                score +="Thirty";
+                break;
+            case 3:
+                score +="Forty";
+                break;
+        }
+        return score;
+    }
+
+    private String closeVictoryOrWining() {
+        String score;
+        int minusResult = player1_Score - player2_Score;
+
+
+        if (minusResult==1) {
+            score = "Advantage player1";
+        }
+        else if (minusResult ==-1) {
+            score = "Advantage player2";
+        }
+        else if (minusResult>=2) {
+            score = "Win for player1";
+        }
+        else {
+            score = "Win for player2";
+        }
+        return score;
+    }
+
+    private String tieScore() {
+        switch (player1_Score)
+        {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
+        }
     }
 }
